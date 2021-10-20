235524a195ea89024d3f61b63aa1d1cafe4c9257
Valerii Kalenyuk <vakabo@gmail.com>
2021-10-21 00:53:32 +0300

    Исправил кнопку вверх, исправил перескок при первой смене участника, добавил автослайдер между участниками

--- src/js/components/footer-modal.js
+++ src/js/components/footer-modal.js
@@ -1,19 +1,40 @@
+import { next } from './footer-slider-btn';
+
+let id = null;
+
 const modal = document.getElementById('myModal');
 const btn = document.getElementById('myBtn');
 const span = document.getElementsByClassName('close')[0];
 btn.onclick = function () {
   modal.style.display = 'block';
   document.body.classList.add('backdrop-scroll');
+  setSliding();
 };
 span.onclick = function () {
   modal.style.display = 'none';
   document.body.classList.remove('backdrop-scroll');
+  if (id) {
+    clearInterval(id);
+  }
 };
 window.onclick = function (event) {
   if (event.target == modal) {
     document.body.classList.remove('backdrop-scroll');
     modal.style.display = 'none';
+    if (id) {
+      clearInterval(id);
+    }
   }
 };
+
+function setSliding() {
+  if (id) {
+    clearInterval(id);
+  }
+
+  id = setInterval(next, 2500);
+}
+
+export { id };
 
 // document.body.classList.remove('backdrop-scroll');
